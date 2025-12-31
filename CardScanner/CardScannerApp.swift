1299e365ad2f8d29c676753d7d7209c0c31e8c77
edwardliu <edwardliu>
2025-12-31 10:31:26 +0800

    Initial Commit

---
+++ CardScanner/CardScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardScannerApp.swift
+//  CardScanner
+//
+//  Created by EdwardLiu on 2025/12/31.
+//
+
+import SwiftUI
+
+@main
+struct CardScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
